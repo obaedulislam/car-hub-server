5aa117afa9e0eedbba250afc7459947bb436740a
obayedul islam <obaed.cse@gmail.com>
2022-11-05 01:09:05 +0600

    Car Hub CRUD Operation Method Called

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try{
         const serviceCollection = client.db('carHub').collection('services');
+
+        const orderCollection = client.db('carHub').collection('orders');
+
         app.get('/services', async(req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -33,6 +36,46 @@ async function run() {
             const query = {_id: ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        })
+
+        //Orders API
+        app.get('/orders', async(req, res) => {
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders)
+        })
+
+        app.post('/orders', async(req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        app.patch('/orders/:id', async(req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = {_id: ObjectId(id)};
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await orderCollection.updateOne(query, updatedDoc);
+            res.send(result);
+
+        })
+
+        app.delete('/orders/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally{
